6e770ccc350a72779979eac901317e27bc713f50
elphep <89947083+elphep@users.noreply.github.com>
2021-11-28 13:14:59 +1300

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -4,7 +4,8 @@ mkdir tempdir
 mkdir tempdir/templates
 mkdir tempdir/static
 
-cp sample_app.py tempdir/.
+cp api_app.py tempdir/.
+cp network_equipment.json tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
@@ -12,11 +13,12 @@ echo "FROM python" >> tempdir/Dockerfile
 echo "RUN pip install flask" >> tempdir/Dockerfile
 echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
-echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
+echo "COPY  api_app.py /home/myapp/" >> tempdir/Dockerfile
+echo "COPY  network_equipment.json /home/myapp/" >> tempdir/Dockerfile
 echo "EXPOSE 5050" >> tempdir/Dockerfile
-echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
+echo "CMD python /home/myapp/api_app.py" >> tempdir/Dockerfile
 
 cd tempdir
-docker build -t sampleapp .
-docker run -t -d -p 5050:5050 --name samplerunning sampleapp
+docker build -t apiapp .
+docker run -t -d -p 5050:5050 --name apiapprunning apiapp
 docker ps -a 
